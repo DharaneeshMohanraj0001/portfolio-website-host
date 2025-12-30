df8a16790d88e89fe6d6772eba1597dcde20c3e3
Dharaneesh Mohanraj <dharaneeshmohanraj0001@gmail.com>
2025-12-30 12:25:42 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -63,3 +63,39 @@ const typed = new Typed('.multiple-text', {
   backDelay: 700,
   loop: true
 });
+const contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    mobile: document.getElementById("mobile").value,
+    subject: document.getElementById("subject").value,
+    message: document.getElementById("message").value,
+  };
+
+  try {
+  const response = await fetch("https://finalone-nv93.onrender.com/api/contact", {
+
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    const result = await response.json();
+
+    alert(result.message);
+
+    if (result.success) {
+      contactForm.reset();
+    }
+  } catch (error) {
+    alert("Something went wrong. Try again later.");
+    console.error(error);
+  }
+});
+
